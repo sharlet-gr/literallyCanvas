326256ae48c5c750f55df75e0d18f3e46497627b
sharlet-gr <swilson@grepruby.com>
2016-09-26 14:21:46 +0530

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var config = {
+  entry: "./App.js",
+
+  output: {
+    filename: "App.js",
+    path: __dirname + "/static",
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ],
+  },
+};
+module.exports = config;
